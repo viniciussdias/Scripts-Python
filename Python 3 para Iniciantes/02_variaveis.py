738ae0f7f74217fc1350bdaacfcb2f2e5b67163c
Vinícius Dias <vinixiussouza@gmail.com>
2020-03-31 13:10:27 -0300

    Adicionado Python 3 para Iniciantes

---
+++ Python 3 para Iniciantes/02_variaveis.py
@@ -0,0 +1,18 @@
+# Variáveis
+
+nome = 'Vinícius Dias'
+idade = 21
+peso = 56.2
+
+print(nome)
+print(type(nome)) # Imprime o tipo da variável nome
+
+print() # Pula uma linha
+
+print(idade)
+print(type(idade)) # Imprime o tipo da variável idade
+
+print() # Pula uma linha
+
+print(peso)
+print(type(peso)) # Imprime o tipo da variável peso
---
+++ Python 3 para Iniciantes/03_tipos_de_dados.py
@@ -0,0 +1,6 @@
+# Tipos de dados
+
+# string (Arara, Carro, Ana)
+# int (0, 255, 1000, -255, -2)
+# float (3.14, 100.20)
+# bool (True, False)
